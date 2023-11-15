35e79365b34f0ab1c8c8ae09aa0f23ec1956a9f9
Игорь Капинкин <i.kapinkin@mail.ru>
2023-11-15 09:09:03 +0300

    Modeling 100 dice rolls, and data analysis

---
+++ Simulation of dice rolls/die_visual.py
@@ -0,0 +1,28 @@
+from die import Die
+from plotly import offline
+from plotly.graph_objs import Bar, Layout
+
+# Создание кубика D6
+die = Die()
+
+# Моделирование серии бросков с сохранением результатов в списке
+results = []
+for roll in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Анализ результатов
+frequencies = []
+for velue in range(1, die.num_sides+1):
+    frequency = results.count(velue)
+    frequencies.append(frequency)
+
+# Визуализация результатов
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {"title": "Result"}
+y_axis_config = {"title": "Frequency of Result"}
+my_layout = Layout(title="Results of rolling one D6 1000 times",
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({"data": data, "layout": my_layout}, filename="d6.html") 
